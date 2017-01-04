68ee320441885801ff5cd07d3d3daeeccae7c7e5
LeCare <francois.caron.perso@gmail.com>
2017-01-04 15:32:14 +0100

    Add sdl port

--- yabause/src/sdl/main.c
+++ yabause/src/sdl/main.c
@@ -19,11 +19,7 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 */
 
-#include <assert.h>
-#if defined(_USEGLEW_)
-#include <GL/glew.h>
-#endif
-#include <GLFW/glfw3.h>
+#include <SDL2/SDL.h>
 
 #include "../yabause.h"
 #include "../gameinfo.h"
@@ -87,6 +83,9 @@ NULL
 
 PerInterface_struct *PERCoreList[] = {
 &PERDummy,
+#ifdef HAVE_LIBSDL
+&PERSDLJoy,
+#endif
 #ifdef ARCH_IS_LINUX
 &PERLinuxJoy,
 #endif
@@ -125,36 +124,102 @@ NULL
 #ifdef YAB_PORT_OSD
 #include "nanovg/nanovg_osdcore.h"
 OSD_struct *OSDCoreList[] = {
-&OSDNnovg,
+&OSDDummy,
 NULL
 };
 #endif
 
+static GLint g_FrameBuffer = 0;
+static GLint g_SWFrameBuffer = 0;
+static GLint g_VertexBuffer = 0;
+static GLint g_VertexDevBuffer = 0;
+static GLint g_VertexSWBuffer = 0;
+static GLint programObject  = 0;
+static GLint positionLoc    = 0;
+static GLint texCoordLoc    = 0;
+static GLint samplerLoc     = 0;
+
+int g_buf_width = -1;
+int g_buf_height = -1;
+
+int fbo_buf_width = -1;
+int fbo_buf_height = -1;
+
+static int resizeFilter = GL_NEAREST;
 static int fullscreen = 0;
 
 static char biospath[256] = "\0";
 static char cdpath[256] = "\0";
 
-GLFWwindow* g_window = NULL;
-GLFWwindow* g_offscreen_context;
+SDL_Window *window;
+SDL_Texture* fb;
+SDL_Renderer* rdr;
 
 yabauseinit_struct yinit;
+
+static int error;
+
+static float vertices [] = {
+   -1.0f, 1.0f, 0, 0,
+   1.0f, 1.0f, 1.0f, 0,
+   1.0f, -1.0f, 1.0f, 1.0f,
+   -1.0f,-1.0f, 0, 1.0f
+};
+
+static float swVertices [] = {
+   -1.0f, 1.0f, 0, 0,
+   1.0f, 1.0f, 1.0f, 0,
+   1.0f, -1.0f, 1.0f, 1.0f,
+   -1.0f,-1.0f, 0, 1.0f
+};
+
+static const float squareVertices [] = {
+   -1.0f, 1.0f, 0, 0,
+   1.0f, 1.0f, 1.0f, 0,
+   1.0f, -1.0f, 1.0f, 1.0f,
+   -1.0f,-1.0f, 0, 1.0f
+};
+
+
+static float devVertices [] = {
+   -1.0f, 1.0f, 0, 0,
+   1.0f, 1.0f, 1.0f, 0,
+   1.0f, -1.0f, 1.0f, 1.0f,
+   -1.0f,-1.0f, 0, 1.0f
+};
 
 void YuiErrorMsg(const char * string) {
     fprintf(stderr, "%s\n\r", string);
 }
 
+void YuiDrawSoftwareBuffer() {
+
+    int buf_width, buf_height;
+    int error;
+
+    VIDCore->GetGlSize(&buf_width, &buf_height);
+   SDL_Rect rect;
+  rect.x = 0;
+  rect.y = 0;
+  rect.w = buf_width;
+  rect.h = buf_height;
+  SDL_Rect dest;
+  dest.x = 0;
+  dest.y = 0;
+  dest.w = WINDOW_WIDTH;
+  dest.h = WINDOW_HEIGHT;
+   SDL_UpdateTexture(fb,&rect,dispbuffer,buf_width*sizeof(pixel_t));
+   SDL_RenderCopy(rdr,fb,&rect,&dest);
+   SDL_RenderPresent(rdr);
+}
+
 int YuiRevokeOGLOnThisThread(){
-#if defined(YAB_ASYNC_RENDERING)
-  glfwMakeContextCurrent(g_offscreen_context);
-#endif
+  // Todo: needs to imp for async rendering
   return 0;
 }
 
 int YuiUseOGLOnThisThread(){
-#if defined(YAB_ASYNC_RENDERING)
-  glfwMakeContextCurrent(g_window);
-#endif
+  // Todo: needs to imp for async rendering
   return 0;
 }
 
@@ -184,25 +249,25 @@ static unsigned long time_left(void)
 
 void YuiSwapBuffers(void) {
 
-   if( g_window == NULL ){
+   if( window == NULL ){
       return;
    }
 
-   glfwSwapBuffers(g_window);
-   SetOSDToggle(1);
-
+   SDL_GL_SwapWindow(window);
   if (frameskip == 1)
     usleep(time_left());
 
   nextFrameTime += delayUs;
+
+   //lastFrameTime = getCurrentTimeUs(0);
 }
 
 void YuiInit() {
 	yinit.m68kcoretype = M68KCORE_MUSASHI;
-	yinit.percoretype = PERCORE_LINUXJOY;
+	yinit.percoretype = PERCORE_SDLJOY;
 	yinit.sh2coretype = SH2CORE_DEFAULT;
 #ifdef FORCE_CORE_SOFT
-  yinit.vidcoretype = VIDCORE_SOFT;
+        yinit.vidcoretype = VIDCORE_SOFT;
 #else
 	yinit.vidcoretype = VIDCORE_OGL; //VIDCORE_SOFT  
 #endif
@@ -215,7 +280,7 @@ void YuiInit() {
 	yinit.buppath = NULL;
 	yinit.mpegpath = NULL;
 	yinit.cartpath = NULL;
-  yinit.videoformattype = VIDEOFORMATTYPE_NTSC;
+        yinit.videoformattype = VIDEOFORMATTYPE_NTSC;
 	yinit.osdcoretype = OSDCORE_DEFAULT;
 	yinit.skip_load = 0;
 
@@ -223,107 +288,71 @@ void YuiInit() {
 	yinit.numthreads = 4;
 }
 
-void error_callback(int error, const char* description)
-{
-  fputs(description, stderr);
-}
+void SDLInit(void) {
+	SDL_GLContext context;
+	Uint32 flags = (fullscreen == 1)?SDL_WINDOW_FULLSCREEN|SDL_WINDOW_OPENGL:SDL_WINDOW_OPENGL;
 
-static int SetupOpenGL() {
-  if (!glfwInit())
-    exit(EXIT_FAILURE);
+	SDL_InitSubSystem(SDL_INIT_VIDEO);
 
-  glfwSetErrorCallback(error_callback);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-  glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API) ;
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  glfwWindowHint(GLFW_RED_BITS,8);
-  glfwWindowHint(GLFW_GREEN_BITS,8);
-  glfwWindowHint(GLFW_BLUE_BITS,8);
+#ifdef _OGL_
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+#endif
 
-  g_window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Yabause", NULL, NULL);
-  if (!g_window)
-  {
-    glfwTerminate();
-    exit(EXIT_FAILURE);
-  }
+	SDL_GL_SetSwapInterval(1);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
-  glfwWindowHint(GLFW_VISIBLE, GL_FALSE);
-  g_offscreen_context = glfwCreateWindow(WINDOW_WIDTH,WINDOW_HEIGHT, "", NULL, g_window);
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 5);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 5);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 5);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 0);
 
-  glfwMakeContextCurrent(g_window);
-  glfwSwapInterval(0);
-  glewExperimental=GL_TRUE;
+	
+	window = SDL_CreateWindow("OpenGL Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, flags);
+	if (!window) {
+    		fprintf(stderr, "Couldn't create window: %s\n", SDL_GetError());
+    		return;
+	}
 
+	context = SDL_GL_CreateContext(window);
+	if (!context) {
+    		fprintf(stderr, "Couldn't create context: %s\n", SDL_GetError());
+    		return;
+	}
+
+	rdr = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+#ifdef _OGL_
+        glClearColor( 0.0f,0.0f,0.0f,1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+#else
+        SDL_RenderClear(rdr);
+#ifdef USE_16BPP
+        fb = SDL_CreateTexture(rdr,SDL_PIXELFORMAT_ABGR1555,SDL_TEXTUREACCESS_STREAMING, 704, 512);
+#else
+        fb = SDL_CreateTexture(rdr,SDL_PIXELFORMAT_ABGR8888,SDL_TEXTUREACCESS_STREAMING, 704, 512);
+#endif
+#endif
+
+  nextFrameTime = getCurrentTimeUs(0) + delayUs;
 }
 
 void displayGameInfo(char *filename) {
-  GameInfo info;
-  if (! GameInfoFromPath(filename, &info))
-  {
-    return;
-  }
+    GameInfo info;
+    if (! GameInfoFromPath(filename, &info))
+    {
+       return;
+    }
 
-  printf("Game Info:\n\tSystem: %s\n\tCompany: %s\n\tItemNum:%s\n\tVersion:%s\n\tDate:%s\n\tCDInfo:%s\n\tRegion:%s\n\tPeripheral:%s\n\tGamename:%s\n", info.system, info.company, info.itemnum, info.version, info.date, info.cdinfo, info.region, info.peripheral, info.gamename);
-}
-
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, GL_TRUE);
-
-  if( action == GLFW_PRESS )
-  {
-    //int yabaky = g_Keymap[key];
-    PerKeyDown(key);
-  }else if( action == GLFW_RELEASE  ){
-    //int yabaky = g_Keymap[key];
-    PerKeyUp(key);
-  }
-}
-
-#define KEYPAD(key, player) ((player << 17)|key)
-
-static void KeyInit() {
-  void * padbits;
-
-  PerPortReset();
-  padbits = PerPadAdd(&PORTDATA1);
-
-  PerSetKey(KEYPAD(PERPAD_UP, 0), PERPAD_UP, padbits);
-  PerSetKey(KEYPAD(PERPAD_RIGHT, 0), PERPAD_RIGHT, padbits);
-  PerSetKey(KEYPAD(PERPAD_DOWN, 0), PERPAD_DOWN, padbits);
-  PerSetKey(KEYPAD(PERPAD_LEFT, 0), PERPAD_LEFT, padbits);
-  PerSetKey(KEYPAD(PERPAD_RIGHT_TRIGGER, 0), PERPAD_RIGHT_TRIGGER, padbits);
-  PerSetKey(KEYPAD(PERPAD_LEFT_TRIGGER, 0), PERPAD_LEFT_TRIGGER, padbits);
-  PerSetKey(KEYPAD(PERPAD_START, 0), PERPAD_START, padbits);
-  PerSetKey(KEYPAD(PERPAD_A, 0), PERPAD_A, padbits);
-  PerSetKey(KEYPAD(PERPAD_B, 0), PERPAD_B, padbits);
-  PerSetKey(KEYPAD(PERPAD_C, 0), PERPAD_C, padbits);
-  PerSetKey(KEYPAD(PERPAD_X, 0), PERPAD_X, padbits);
-  PerSetKey(KEYPAD(PERPAD_Y, 0), PERPAD_Y, padbits);
-  PerSetKey(KEYPAD(PERPAD_Z, 0), PERPAD_Z, padbits);
-
-  padbits = PerPadAdd(&PORTDATA2);
-
-  PerSetKey(KEYPAD(PERPAD_UP, 1), PERPAD_UP, padbits);
-  PerSetKey(KEYPAD(PERPAD_RIGHT, 1), PERPAD_RIGHT, padbits);
-  PerSetKey(KEYPAD(PERPAD_DOWN, 1), PERPAD_DOWN, padbits);
-  PerSetKey(KEYPAD(PERPAD_LEFT, 1), PERPAD_LEFT, padbits);
-  PerSetKey(KEYPAD(PERPAD_RIGHT_TRIGGER, 1), PERPAD_RIGHT_TRIGGER, padbits);
-  PerSetKey(KEYPAD(PERPAD_LEFT_TRIGGER, 1), PERPAD_LEFT_TRIGGER, padbits);
-  PerSetKey(KEYPAD(PERPAD_START, 1), PERPAD_START, padbits);
-  PerSetKey(KEYPAD(PERPAD_A, 1), PERPAD_A, padbits);
-  PerSetKey(KEYPAD(PERPAD_B, 1), PERPAD_B, padbits);
-  PerSetKey(KEYPAD(PERPAD_C, 1), PERPAD_C, padbits);
-  PerSetKey(KEYPAD(PERPAD_X, 1), PERPAD_X, padbits);
-  PerSetKey(KEYPAD(PERPAD_Y, 1), PERPAD_Y, padbits);
-  PerSetKey(KEYPAD(PERPAD_Z, 1), PERPAD_Z, padbits);
-
+    printf("Game Info:\n\tSystem: %s\n\tCompany: %s\n\tItemNum:%s\n\tVersion:%s\n\tDate:%s\n\tCDInfo:%s\n\tRegion:%s\n\tPeripheral:%s\n\tGamename:%s\n", info.system, info.company, info.itemnum, info.version, info.date, info.cdinfo, info.region, info.peripheral, info.gamename);
 }
 
 int main(int argc, char *argv[]) {
 	int i;
+	SDL_Event event;
+	int quit = SDL_FALSE;
+	SDL_GameController* ctrl;
 
 	LogStart();
 	LogChangeOutput( DEBUG_STDERR, NULL );
@@ -331,101 +360,110 @@ int main(int argc, char *argv[]) {
 	YuiInit();
 
 //handle command line arguments
-  for (i = 1; i < argc; ++i) {
-    if (argv[i]) {
-      //show usage
-      if (0 == strcmp(argv[i], "-h") || 0 == strcmp(argv[i], "-?") || 0 == strcmp(argv[i], "--help")) {
-        print_usage(argv[0]);
-        return 0;
-      }
+   for (i = 1; i < argc; ++i) {
+      if (argv[i]) {
+         //show usage
+         if (0 == strcmp(argv[i], "-h") || 0 == strcmp(argv[i], "-?") || 0 == strcmp(argv[i], "--help")) {
+            print_usage(argv[0]);
+            return 0;
+         }
 			
-      //set bios
-      if (0 == strcmp(argv[i], "-b") && argv[i + 1]) {
-        strncpy(biospath, argv[i + 1], 256);
-        yinit.biospath = biospath;
-      } else if (strstr(argv[i], "--bios=")) {
-        strncpy(biospath, argv[i] + strlen("--bios="), 256);
-        yinit.biospath = biospath;
-      }
-      //set iso
-      else if (0 == strcmp(argv[i], "-i") && argv[i + 1]) {
-        strncpy(cdpath, argv[i + 1], 256);
-        yinit.cdcoretype = 1;
-        yinit.cdpath = cdpath;
-        displayGameInfo(cdpath);
-      } else if (strstr(argv[i], "--iso=")) {
-        strncpy(cdpath, argv[i] + strlen("--iso="), 256);
-        yinit.cdcoretype = 1;
-        yinit.cdpath = cdpath;
-      }
-      //set cdrom
-      else if (0 == strcmp(argv[i], "-c") && argv[i + 1]) {
-        strncpy(cdpath, argv[i + 1], 256);
-        yinit.cdcoretype = 2;
-        yinit.cdpath = cdpath;
-      } else if (strstr(argv[i], "--cdrom=")) {
-        strncpy(cdpath, argv[i] + strlen("--cdrom="), 256);
-        yinit.cdcoretype = 2;
-        yinit.cdpath = cdpath;
-      }
-      // Set sound
-      else if (strcmp(argv[i], "-ns") == 0 || strcmp(argv[i], "--nosound") == 0) {
-        yinit.sndcoretype = 0;
-      }
-      // Set sound
-      else if (strcmp(argv[i], "-f") == 0 || strcmp(argv[i], "--fullscreen") == 0) {
-        fullscreen = 1;
-      }
-      else if (strcmp(argv[i], "-sc") == 0 || strcmp(argv[i], "--softcore") == 0) {
-        yinit.vidcoretype = VIDCORE_SOFT;
-      }
-      // Auto frame skip
-      else if (strstr(argv[i], "--vsyncoff")) {
-        frameskip = 0;
-      }
-      // Binary
-      else if (strstr(argv[i], "--binary=")) {
-        char binname[1024];
-        unsigned int binaddress;
-        int bincount;
+         //set bios
+         if (0 == strcmp(argv[i], "-b") && argv[i + 1]) {
+            strncpy(biospath, argv[i + 1], 256);
+            yinit.biospath = biospath;
+	 } else if (strstr(argv[i], "--bios=")) {
+            strncpy(biospath, argv[i] + strlen("--bios="), 256);
+            yinit.biospath = biospath;
+	 }
+         //set iso
+         else if (0 == strcmp(argv[i], "-i") && argv[i + 1]) {
+            strncpy(cdpath, argv[i + 1], 256);
+	    yinit.cdcoretype = 1;
+	    yinit.cdpath = cdpath;
+	    displayGameInfo(cdpath);
+	 } else if (strstr(argv[i], "--iso=")) {
+            strncpy(cdpath, argv[i] + strlen("--iso="), 256);
+	    yinit.cdcoretype = 1;
+	    yinit.cdpath = cdpath;
+	 }
+         //set cdrom
+	 else if (0 == strcmp(argv[i], "-c") && argv[i + 1]) {
+            strncpy(cdpath, argv[i + 1], 256);
+	    yinit.cdcoretype = 2;
+	    yinit.cdpath = cdpath;
+	 } else if (strstr(argv[i], "--cdrom=")) {
+            strncpy(cdpath, argv[i] + strlen("--cdrom="), 256);
+	    yinit.cdcoretype = 2;
+	    yinit.cdpath = cdpath;
+	 }
+         // Set sound
+         else if (strcmp(argv[i], "-ns") == 0 || strcmp(argv[i], "--nosound") == 0) {
+	    yinit.sndcoretype = 0;
+	 }
+         // Set sound
+         else if (strcmp(argv[i], "-f") == 0 || strcmp(argv[i], "--fullscreen") == 0) {
+	    fullscreen = 1;
+	 }
+         // Set sound
+         else if (strcmp(argv[i], "-rb") == 0 || strcmp(argv[i], "--resizebilinear") == 0) {
+	    resizeFilter = GL_LINEAR;
+	 }
+	 else if (strcmp(argv[i], "-sc") == 0 || strcmp(argv[i], "--softcore") == 0) {
+	    yinit.vidcoretype = VIDCORE_SOFT;
+	 }
+         // Auto frame skip
+         else if (strstr(argv[i], "--vsyncoff")) {
+              frameskip = 0;
+         }
+	 // Binary
+	 else if (strstr(argv[i], "--binary=")) {
+	    char binname[1024];
+	    unsigned int binaddress;
+	    int bincount;
 
-        bincount = sscanf(argv[i] + strlen("--binary="), "%[^:]:%x", binname, &binaddress);
-        if (bincount > 0) {
-          if (bincount < 2) binaddress = 0x06004000;
-          MappedMemoryLoadExec(binname, binaddress);
-        }
-      }
-    }
-  }
-  SetupOpenGL();
+	    bincount = sscanf(argv[i] + strlen("--binary="), "%[^:]:%x", binname, &binaddress);
+	    if (bincount > 0) {
+	       if (bincount < 2) binaddress = 0x06004000;
 
-  glfwSetKeyCallback(g_window, key_callback);
+               //yui_window_run(YUI_WINDOW(yui));
+	       MappedMemoryLoadExec(binname, binaddress);
+	    }
+	 }
+      }
+   }
+        SDLInit();
 
 	YabauseDeInit();
 
-  if (YabauseInit(&yinit) != 0) printf("YabauseInit error \n\r");
-
-  KeyInit();
+        if (YabauseInit(&yinit) != 0) printf("YabauseInit error \n\r");
 
   if (yinit.vidcoretype == VIDCORE_OGL) {
-    VIDCore->SetSettingValue(VDP_SETTING_RESOLUTION_MODE, RES_NATIVE);
-    VIDCore->SetSettingValue(VDP_SETTING_FILTERMODE, AA_FXAA);
+    VIDCore->SetSettingValue(VDP_SETTING_RESOLUTION_MODE, RES_ORIGINAL);
     VIDCore->Resize(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 1);
   }
 
-  nextFrameTime = getCurrentTimeUs(0) + delayUs;
+	while (quit == SDL_FALSE)
+	{
+		if (SDL_PollEvent(&event))
+		{
+   			switch(event.type)
+    			{
+     				case SDL_QUIT:
+      					quit = SDL_TRUE;
+      				break;
+				case SDL_KEYDOWN:
+					printf("Key down!");
+				break;
+     			}
+  		}
 
-  while (!glfwWindowShouldClose(g_window))
-  {
-        if (PERCore->HandleEvents() == -1) glfwSetWindowShouldClose(g_window, GL_TRUE);
-        glfwPollEvents();
-  }
+	        PERCore->HandleEvents();
+	}
 
 	YabauseDeInit();
 	LogStop();
-  glfwDestroyWindow(g_window);
-  glfwDestroyWindow(g_offscreen_context);
-  glfwTerminate();
+	SDL_Quit();
 
 	return 0;
 }
